63a66c5aec3f0b0f1d44c9a24cad54430dfe1a4f
Deehdee <dallasdorries99@gmail.com>
2024-11-14 12:40:56 -0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,23 @@
 * Formatted some comments so that each section is seperated by a green text header-comment
 * Copied the code form the MPU6050 example and assigned each acceleration and gryo value to a double value instead of the called value being printed directly.
 * Code currently runs at a delay of 300 mills, idealy, the code will run with a shorter delay for the PID controller to be accurate
-
+* Solved an issue where servos were not communicating with Teensy.
+* -When using Arduino IDE, servo behave normally using Servo.h library, but not when using VSCode
+* -When using PWMServo.h library, servos behave normally
 */
+//TODO: Find the differences between the two MPU variables and their libraries 
+//TODO Find out what Compute() function does
+//TODO Find out what tsetTunings() function does
+//TODO Research how to calculate gains for PID Controller
+//TODO Impliment Kalman Filter - one that works properly
+
 // *-------------------Includes------------------- //
-#include <Arduino.h>
-#include <Servo.h> // Servo library
+#include <Arduino.h> //!Is this one necessary?
+#include <PWMServo.h> // Servo library, PWM library works for Teensy in VSCode
 #include <PID_v1.h> // Custom PID library, in favor of using legacy PID library
 #include <Adafruit_MPU6050.h> // MPU library from Adafruit
 #include <Wire.h> //Library for I2C
+#include <KalmanFilter.h> //!Figure this out
 
 // *-------------------Variables------------------- //
 unsigned long lastTime;
@@ -20,20 +29,25 @@ double errSum, lastErr;
 double kp, ki, kd;
 
 // setup servos 
-Servo longServo; // Defines longServo object
-Servo latServo; // Defines latServo object
+PWMServo longServo; // Defines longServo object
+PWMServo latServo; // Defines latServo object
 int longPin = 28;
 int latPin = 29;
 
 // Setup gyro/accel
-//TODO: Find the differences between the two MPU variables and their libraries 
 Adafruit_MPU6050 mpu;
 
 //Setup Kalman Filter Variables
+KalmanFilter kalmanX(0.001, 0.003, 0.03);
+KalmanFilter kalmanY(0.001, 0.003, 0.03);
 
+float accPitch = 0;
+float accRoll = 0;
+
+float kalPitch = 0;
+float kalRoll = 0;
 
 // *-------------------Functions------------------- //
-//TODO Find out what this function does
 void Compute(){
   // How long since last calculated
   unsigned long now = millis();
@@ -57,7 +71,7 @@ void Compute(){
   Serial.println(now/1000);
   Serial.println("");
 }
-//TODO Find out what this function does
+
 void setTunings(double Kp, double Ki, double Kd){
   kp = Kp;
   ki = Ki;
@@ -70,6 +84,9 @@ void getMPU(){
 /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
    mpu.getEvent(&a, &g, &temp);
+  
+  //kalPitch = kalmanY.update(accPitch, gyr.YAxis);
+  //kalRoll = kalmanX.update(accRoll, gyr.XAxis);
 
   // Set values to 
   double ax = a.acceleration.x-0.24;
@@ -112,13 +129,16 @@ void setup() {
   Wire.begin();
 
   // Wait for serial monitor begin 
-  while (!Serial){
+  /*while (!Serial){
     Serial.println("Waiting...");
     delay(20);}
+    */
 
   // Attach to servos to their respective pins
   longServo.attach(longPin);
   latServo.attach(latPin);
+  Serial.println("Long and Lat Servos attached.");
+  delay(100);
 
   //Calls IMU to initialize, freezes program if it isn't successful
   if (!mpu.begin()) {
@@ -128,16 +148,23 @@ void setup() {
     }
   }
   else if (mpu.begin()){
-    Serial.print("mpu ready!");
+    Serial.println("MPU ready!");
+    delay(100);
   }
   //Configure the MPU
   //These settings can be changed, look at the example for the different configurations
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("MPU Configured: 8Gs - 500 DEG - 21HZ");
+
 }
 // *-------------------Loop------------------- //
 void loop() {
   getMPU();
   Compute();
+  longServo.write(90);
+  delay(1000);
+  longServo.write(0);
+  delay(1000);
 }
